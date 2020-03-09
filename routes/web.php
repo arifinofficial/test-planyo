33c5b00b0d8963430b1408e96c948df4ba0214d7
Arifin N <arifinofficial30@gmail.com>
2020-03-09 20:20:22 +0800

    update planyo

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/planyo/reservation', 'HomeController@reservation')->name('planyo.newReservation');
 Route::post('/planyo/reservation-confirm', 'HomeController@reservationConfirm')->name('planyo.newReservationConfirm');
 
